687816e69d49b3347bc5755c50d897154cf76aa7
DeeprajGawand <deeprajgawand@gmail.com>
2024-01-22 15:48:10 +0530

    added validation to contact card

--- src/components/AddAndUpdateContact.jsx
+++ src/components/AddAndUpdateContact.jsx
@@ -1,9 +1,15 @@
-import {Field, Form, Formik} from "formik";
+import {ErrorMessage, Field, Form, Formik} from "formik";
 import Modal from './Modal';
 import { addDoc, collection, doc, updateDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import * as Yup from "yup";
+
+const contactSchemaValidation = Yup.object().shape({
+  name: Yup.string().required("Name is Required"),
+  email: Yup.string().email("Invalid Email").required("Email is Required"),
+});
 
 const AddAndUpdateContact = ({onClose, isOpen, isUpdate, contact}) => {
   const addContact = async (contact) => {
@@ -31,6 +37,7 @@ const AddAndUpdateContact = ({onClose, isOpen, isUpdate, contact}) => {
     <div>
     <Modal isOpen={isOpen} onClose={onClose}>
       <Formik
+      validationSchema={contactSchemaValidation}
       initialValues={isUpdate
          ? {
         name: contact.name,
@@ -52,11 +59,17 @@ const AddAndUpdateContact = ({onClose, isOpen, isUpdate, contact}) => {
             <div className="flex flex-col gap-1">
                 <label htmlFor="name">Name</label>
                 <Field name ="name" className=" h-10 border"/>
+                <div className="text-xs text-red-500">
+                  <ErrorMessage name="name"/>
+                </div>
             </div>
             <div className="flex flex-col gap-1">
                 <label htmlFor="email">Email</label>
                 <Field 
                  name ="email" className=" h-10 border"/>
+                 <div className="text-xs text-red-500">
+                  <ErrorMessage name="email"/>
+                </div>
             </div>
 
             <button className=" self-end border bg-orange px-3 py-1.5">
