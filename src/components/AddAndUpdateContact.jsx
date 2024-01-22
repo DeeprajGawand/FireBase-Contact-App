69d07499853d3de547ca544ada35c1fb710c30f6
DeeprajGawand <deeprajgawand@gmail.com>
2024-01-22 10:13:04 +0530

    added input fields and add functionality in AddUpadteContact

--- src/components/AddAndUpdateContact.jsx
+++ src/components/AddAndUpdateContact.jsx
@@ -1,14 +1,52 @@
-import React from 'react'
-import Modal from './Modal'
+import {Field, Form, Formik} from "formik";
+import Modal from './Modal';
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 const AddAndUpdateContact = ({onClose, isOpen}) => {
+  const addContact = async (contact) => {
+    try {
+        const contactRef = collection(db, "contacts");
+        await addDoc(contactRef, contact)   
+    } catch (error) {
+       console.log(error); 
+    }
+  };
+
   return (
     <div>
     <Modal isOpen={isOpen} onClose={onClose}>
-      Hi Everyone
+      <Formik
+      initialValues={{
+        name:"",
+        email: "",
+      }}
+      onSubmit={(values)=>{
+        console.log(values);
+        addContact(values);
+      }}
+
+
+      >
+        <Form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-1">
+                <label htmlFor="name">Name</label>
+                <Field name ="name" className=" h-10 border"/>
+            </div>
+            <div className="flex flex-col gap-1">
+                <label htmlFor="email">Email</label>
+                <Field 
+                 name ="email" className=" h-10 border"/>
+            </div>
+
+            <button className=" self-end border bg-orange px-3 py-1.5">
+                add contact
+                </button>
+        </Form>
+      </Formik>
     </Modal>
    </div>
   )
 }
 
-export default AddAndUpdateContact
+export default AddAndUpdateContact;
