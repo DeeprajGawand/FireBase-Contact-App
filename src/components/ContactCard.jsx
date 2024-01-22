2ae3e4a69f6703bb8e5e18a67208e81df270d433
DeeprajGawand <deeprajgawand@gmail.com>
2024-01-22 10:49:11 +0530

    added delete functionality

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 import { RiEditCircleLine } from 'react-icons/ri'
 import {IoMdTrash} from "react-icons/io"
 import { HiOutlineUserCircle } from 'react-icons/hi'
+import { deleteDoc, doc } from 'firebase/firestore'
+import { db } from '../config/firebase'
 
 const ContactCard = ({contact}) => {
+
+  const deleteContact = async (id) =>{
+    try {
+      await deleteDoc(doc(db, "contacts", id));
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
   return (
     <div key={contact.id} className="bg-yellow flex
           justify-between items-center p-2 rounded-lg ">
@@ -16,7 +27,9 @@ const ContactCard = ({contact}) => {
            </div>
             <div className="flex text-3xl">
               <RiEditCircleLine  />
-              <IoMdTrash  className="text-orange"/>
+              <IoMdTrash  
+              onClick={()=> deleteContact(contact.id)}
+              className="text-orange"/>
             </div>
           </div>
   )
